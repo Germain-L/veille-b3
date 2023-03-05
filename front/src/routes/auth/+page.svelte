e999a330d3688af52157db87a486b7e6fcc00a6d
Germain Leignel <germain.leignel@gmail.com>
2023-03-05 18:54:24 +0100

    register

--- front/src/routes/auth/+page.svelte
+++ front/src/routes/auth/+page.svelte
@@ -30,13 +30,13 @@
 			>
 				Log In
 			</button>
-			<!-- <button
+			<button
 				type="submit"
 				class="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
 				formaction="?/register"
 			>
 				Register
-			</button> -->
+			</button>
 		</div>
 
 		{#if form?.error}<p id="error-message" class="text-center text-red-500 ">
